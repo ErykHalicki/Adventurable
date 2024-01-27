31bc9515730d7379e58e9e18b240c2a24e420b9d
Eryk Halicki <erykhalicki0@gmail.com>
2024-01-27 15:20:53 -0800

    models fix

--- website/myapp/models.py
+++ website/myapp/models.py
@@ -1,5 +1,2 @@
 from django.db import models
 
-class UserResponse(models.Model):
-    name = models.CharField(max_length=255)
-    age = models.IntegerField()
--- website/myapp/views.py
+++ website/myapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import UserResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
