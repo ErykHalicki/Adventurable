abca6cd3519b6f3802faf9ca8e5ec35e9b69c719
Eryk Halicki <erykhalicki0@gmail.com>
2024-01-27 19:48:45 -0800

    google api success

--- website/myapp/views.py
+++ website/myapp/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import googlemaps
+
+mapsClient = googlemaps.Client("AIzaSyAJJ_vstzJNHUQtkYZYRvZqyBt-x0sqM8A")
+
+def getGoogleLocation(location):
+    result= mapsClient.find_place(input=location, input_type="textquery", fields=["name","place_id","photos"] )["candidates"][0]
+    f = open("test.jpg", 'wb')
+    photo_reference=result['photos'][0]['photo_reference']
+    for chunk in mapsClient.places_photo(photo_reference, max_width=800):
+        if chunk:
+            f.write(chunk)
+    f.close()
+    return result
 
 @csrf_exempt
 def submit_form(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        for d in data:
-            print(d)
-        print(data)
+        print(getGoogleLocation("Algonquin Park"))
         return JsonResponse({'status': 'success'})
     return JsonResponse({'status': 'error'})
 
